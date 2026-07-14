69fd30048165a1afed6cc1f6c0ccf57193509132
shahlaMoh <xartsho1x@gmail.com>
2026-07-15 01:50:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-// سيتم ربط بيانات Excel هنا لاحقًا
-console.log("Dental Dashboard Loaded");
+fetch("data.json")
+.then(response => response.json())
+.then(data => {
+    document.getElementById("dashboard").innerHTML = `
+        <h2>Dashboard Connected Successfully</h2>
+        <p>Total Patients: ${data.totalPatients}</p>
+        <p>Total Revenue: ${data.totalRevenue} SAR</p>
+    `;
+});
